850c4163ad18de8d24de0fe8dc4021902e0bbabf
litaff <a.gazda2k18@gmail.com>
2023-02-13 16:12:18 +0100

    Added teleport pad
    Added teleport pad which teleports to a linked pad
    Linking is done by adding reference in editor
    Offset to move characters is configurable

---
+++ Let Me Out/Assets/Scripts/Trigger Able/TeleporterPad.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class TeleporterPad : MonoBehaviour
+{
+    [SerializeField] private TeleporterPad linkedPad;
+    [SerializeField] private int teleportOffset;
+    private List<Character> characters;
+
+    private void Awake()
+    {
+        characters = new List<Character>();
+        GetComponent<CircleCollider2D>().isTrigger = true; // to make sure it's a trigger
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        var character = col.GetComponent<Character>();
+        
+        if(characters.Contains(character)) return;
+        
+        linkedPad.Teleport(character);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var character = other.GetComponent<Character>();
+
+        characters.Remove(character);
+    }
+
+    private void Teleport(Character character)
+    {
+        characters.Add(character);
+        var position = transform.position;
+        character.transform.position = new Vector2(position.x, position.y + teleportOffset);
+    }
+}
