a7f206ca5f4415d5b0c0018af8280c1257c57959
litaff <a.gazda2k18@gmail.com>
2023-02-13 19:19:45 +0100

    Added lever
    Triggers Activate OnTriggerEnter2D when Active state was false
    Triggers Deactivate OnTriggerEnter2D when Active state was true

---
+++ Let Me Out/Assets/Scripts/Activators/Lever.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace Activators
+{
+    /// <summary>
+    /// Becomes active OnTriggerEnter2D and inactive on the second OnTriggerEnter2D
+    /// </summary>
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class Lever : MonoBehaviour
+    {
+        public static event Action<int> Activate;
+        public static event Action<int> Deactivate;
+
+        public bool Active { get; private set; }
+    
+        [SerializeField] protected int id;
+
+        private void Awake()
+        {
+            GetComponent<CircleCollider2D>().isTrigger = true;
+            Active = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            var character = col.GetComponent<Character>();
+        
+            if(character is null) return;
+
+            if (Active)
+            {
+                Deactivate?.Invoke(id);
+                Active = false;
+                return;
+            }
+        
+            Activate?.Invoke(id);
+            Active = true;
+        }
+    }
+}
