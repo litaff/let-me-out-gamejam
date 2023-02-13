0ac03f0d3186e346e613a80191e755e219ad917a
litaff <a.gazda2k18@gmail.com>
2023-02-13 19:23:48 +0100

    Support for different activators + fixed
    Fixed doors closing while opening, caused by delay with closing and multiple closing sequences after a delay
    Added support for different activators

--- Let Me Out/Assets/Scripts/Trigger Able/AutomaticDoor.cs
+++ Let Me Out/Assets/Scripts/Trigger Able/AutomaticDoor.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Activators;
+using TreeEditor;
 using UnityEngine;
 
 public class AutomaticDoor : MonoBehaviour
 {
+    [SerializeField] private ActivatorType activatorType;
     [SerializeField] private int id;
     [SerializeField] private float doActionFor;
     [SerializeField] private float keepActionFor;
@@ -12,39 +15,53 @@ public class AutomaticDoor : MonoBehaviour
     private bool isMoving;
     private float time;
     private Vector2 startPos;
-    private Vector2 endPos;
+    private Vector2 startLerp;
+    private Vector2 endLerp;
 
     private void Awake()
     {
-        PressurePlate.Activate += Open;
-        PressurePlate.Deactivate += Close;
+        isMoving = false;
+        startPos = transform.position;
+        
+        switch (activatorType)
+        {
+            case ActivatorType.Lever:
+                Lever.Activate += Open;
+                Lever.Deactivate += Close;
+                break;
+            case ActivatorType.PressurePlate:
+                PressurePlate.Activate += Open;
+                PressurePlate.Deactivate += Close;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 
     private void Open(int activatorId)
     {
-        if(activatorId != id) return;
-
+        if (activatorId != id) return;
         StartCoroutine(WaitAndSetFor(true, 0f));
     }
 
     private void Close(int activatorId)
     {
-        if(activatorId != id) return;
-
+        if (activatorId != id) return;
         StartCoroutine(WaitAndSetFor(false, keepActionFor));
     }
 
     private IEnumerator WaitAndSetFor(bool up, float delay)
     {
         yield return new WaitForSeconds(delay);
-        
+        if (isMoving && delay > 0.1f) yield break;
         isMoving = true;
         time = 0;
-        
-        startPos = transform.position;
-        endPos = new Vector2(
-            startPos.x + positionDelta.x * (up ? 1 : -1), 
-            startPos.y + positionDelta.y * (up ? 1 : -1));
+
+        startLerp = startPos + positionDelta * (up ? 0 : 1);
+        endLerp = startLerp + positionDelta * (up ? 1 : -1);
+
+        var progress = ((Vector2) transform.position - endLerp).magnitude / positionDelta.magnitude;
+        time = doActionFor - doActionFor * progress;
     }
     
     private void Update()
@@ -53,8 +70,9 @@ public class AutomaticDoor : MonoBehaviour
         
         time += Time.deltaTime;
 
-        transform.position = Vector2.Lerp(startPos, endPos, time / doActionFor);
+        transform.position = Vector2.Lerp(startLerp, endLerp, time / doActionFor);
 
-        if (time >= doActionFor) isMoving = false;
+        if (!(time >= doActionFor)) return;
+        isMoving = false;
     }
 }
